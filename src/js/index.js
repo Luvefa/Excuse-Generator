e74620cf7e01cefc9a1e9002a6fd5f848e2bb43e
Luvefa <luis.velazco001@mymdc.net>
2020-10-13 05:03:58 +0000

    first commitn

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,36 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#Excuse").innerHTML = generateExcuse();
+  });
+
+  let generateExcuse = () => {
+    let who = ["The dog", "My grandma", "My turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whoIndx = Math.floor(Math.random() * who.length);
+    let actionIndx = Math.floor(Math.random() * action.length);
+    let whatIndx = Math.floor(Math.random() * what.length);
+    let whenIndx = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoIndx] +
+      " " +
+      action[actionIndx] +
+      " " +
+      what[whatIndx] +
+      " " +
+      when[whenIndx]
+    );
+  };
 };
